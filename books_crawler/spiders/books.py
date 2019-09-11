c0588daf0b783da6209fabddc0fb80aaf71db555
hamid-shahmohammadi <hamid.shahmohamadi@gmail.com>
2019-09-11 11:01:03 +0430

    lesson-26

--- books_crawler/spiders/books.py
+++ books_crawler/spiders/books.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+from time import sleep
 from scrapy import Spider
 from selenium import webdriver
 from scrapy.selector import Selector
 from scrapy.http import Request
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BooksSpider(Spider):
@@ -19,6 +21,24 @@ class BooksSpider(Spider):
         for book in books:
             url='http://books.toscrape.com/' + book
             yield Request(url,callback=self.parse_book)
+
+        while True:
+            try:
+                next_page=self.driver.find_element_by_xpath('//a[text()="next"]')
+                sleep(3)
+                self.logger.info('Sleeping for 3 seconds')
+                next_page.click()
+                sel = Selector(text=self.driver.page_source)
+                books = sel.xpath('//h3/a/@href').extract()
+
+                for book in books:
+                    url = 'http://books.toscrape.com/catalogue/' + book
+                    yield Request(url, callback=self.parse_book)
+
+            except NoSuchElementException:
+                self.logger.info('No more page to load')
+                self.driver.quit()
+                break
 
 
     def parse_book(self,response):
