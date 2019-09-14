772726ef42456a7659d5a644f277e33598c6a1cb
hamid-shahmohammadi <hamid.shahmohamadi@gmail.com>
2019-09-14 11:42:28 +0430

    lesson-28

--- books_crawler/spiders/books.py
+++ books_crawler/spiders/books.py
@@ -1,44 +1,23 @@
 # -*- coding: utf-8 -*-
-from time import sleep
-from scrapy import Spider
-from selenium import webdriver
-from scrapy.selector import Selector
-from scrapy.http import Request
-from selenium.common.exceptions import NoSuchElementException
 
+from scrapy import Spider
+from scrapy.http import Request
 
 class BooksSpider(Spider):
     name = 'books'
     allowed_domains = ['books.toscrape.com']
+    start_urls=['http://books.toscrape.com']
 
-    def start_requests(self):
-        self.driver=webdriver.Chrome('/home/djangoadmin/Desktop/Software/chromedriver')
-        self.driver.get('http://books.toscrape.com/')
 
-        sel = Selector(text=self.driver.page_source)
-        books=sel.xpath('//h3/a/@href').extract()
-
+    def parse(self,response):
+        books = response.xpath('//h3/a/@href').extract()
         for book in books:
-            url='http://books.toscrape.com/' + book
-            yield Request(url,callback=self.parse_book)
+            absolute_url=response.urljoin(book)
+            yield Request(absolute_url,callback=self.parse_book)
 
-        while True:
-            try:
-                next_page=self.driver.find_element_by_xpath('//a[text()="next"]')
-                sleep(3)
-                self.logger.info('Sleeping for 3 seconds')
-                next_page.click()
-                sel = Selector(text=self.driver.page_source)
-                books = sel.xpath('//h3/a/@href').extract()
-
-                for book in books:
-                    url = 'http://books.toscrape.com/catalogue/' + book
-                    yield Request(url, callback=self.parse_book)
-
-            except NoSuchElementException:
-                self.logger.info('No more page to load')
-                self.driver.quit()
-                break
+        next_page_url = response.xpath('//a[text()="next"]/@href').extract_first()
+        absolute_next_page_url = response.urljoin(next_page_url)
+        yield Request(absolute_next_page_url)
 
 
     def parse_book(self,response):
